e285d5e4bc1096228da1409db0fac4b76403a733
Gowtham Behara <gothsiddu@gmail.com>
2019-10-08 10:26:18 +0530

    Show error is input fields are empty

--- server/helper/validation/user-register.js
+++ server/helper/validation/user-register.js
@@ -9,23 +9,23 @@ module.exports = (data) => {
     data.password = isEmpty(data.password) ? "" : data.password;
     data.confirm_password = isEmpty(data.confirm_password) ? "" : data.confirm_password;
 
-    if (!validator.isEmpty(data.name)) {
+    if (validator.isEmpty(data.name)) {
         errors.name = "Name is required";
     }
 
-    if (!validator.isEmpty(data.email)) {
+    if (validator.isEmpty(data.email)) {
         errors.email = "Name is required";
     } else if (!validator.isEmail(data.email)) {
         errors.email = "Email is invalid";
     }
 
-    if (!validator.isEmpty(data.password)) {
+    if (validator.isEmpty(data.password)) {
         errors.password = "Password is required";
     } else if (!validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = "Password must be min of 6 chars and max of 30 chars";
     }
 
-    if (!validator.isEmpty(data.confirm_password)) {
+    if (validator.isEmpty(data.confirm_password)) {
         errors.confirm_password = "Password is required";
     } else if (!validator.isLength(data.confirm_password, { min: 6, max: 30 })) {
         errors.confirm_password = "Password must be min of 6 chars and max of 30 chars";
