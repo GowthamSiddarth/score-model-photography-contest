79b70dcd0461cb6b7c223377304888092fce1467
Gowtham Behara <gothsiddu@gmail.com>
2019-10-08 10:16:50 +0530

    Define func to validate user-register input

---
+++ server/helper/validation/user-register.js
@@ -0,0 +1,38 @@
+const validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = (data) => {
+    let errors = {};
+
+    data.name = isEmpty(data.name) ? "" : data.name;
+    data.email = isEmpty(data.email) ? "" : data.email;
+    data.password = isEmpty(data.password) ? "" : data.password;
+    data.confirm_password = isEmpty(data.confirm_password) ? "" : data.confirm_password;
+
+    if (!validator.isEmpty(data.name)) {
+        errors.name = "Name is required";
+    }
+
+    if (!validator.isEmpty(data.email)) {
+        errors.email = "Name is required";
+    } else if (!validator.isEmail(data.email)) {
+        errors.email = "Email is invalid";
+    }
+
+    if (!validator.isEmpty(data.password)) {
+        errors.password = "Password is required";
+    } else if (!validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = "Password must be min of 6 chars and max of 30 chars";
+    }
+
+    if (!validator.isEmpty(data.confirm_password)) {
+        errors.confirm_password = "Password is required";
+    } else if (!validator.isLength(data.confirm_password, { min: 6, max: 30 })) {
+        errors.confirm_password = "Password must be min of 6 chars and max of 30 chars";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+};
