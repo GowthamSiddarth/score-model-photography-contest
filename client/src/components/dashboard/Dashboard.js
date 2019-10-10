4886dda662dbbab56e9229aed27f2d19e3d12fc1
Gowtham Behara <gothsiddu@gmail.com>
2019-10-10 18:36:08 +0530

    Create component for Dashboard and connect to redux store manager

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { bindActionCreators } from "redux";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { logoutUser } from "../../utils/redux/actions/authActions";
+
+class Dashboard extends Component {
+
+    onLogoutClick(e) {
+        e.preventDefault();
+        this.props.logoutUser(this.props.history);
+    }
+
+    render() {
+        const { user } = this.props.auth;
+
+        return (
+            <div style={{ height: "75vh" }} className="container valign-wrapper">
+                <div className="row" style={{ paddingTop: "20px" }}>
+                    <div className="col s12 center-align">
+                        <h4>
+                            <b>Hey there,</b> {user.name.split(" ")[0]}
+                            <p className="flow-text grey-text text-darken-1">
+                                You're logged into
+                                <span style={{ fontFamily: "monospace" }}>Photography Scoring Model Contest</span>
+                            </p>
+                        </h4>
+                        <button
+                            style={{
+                                width: "150px",
+                                borderRadius: "3px",
+                                letterSpacing: "1.5px",
+                                marginTop: "1rem"
+                            }}
+                            onClick={this.onLogoutClick}
+                            className="btn btn-large waves-effect waves-light hoverable yellow accent-4">
+                            Logout
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+Dashboard.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+const mapDispatchToProps = dispatch => ({
+    logoutUser: bindActionCreators(logoutUser, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Dashboard));
