812cf95d3a1d488817b0d4b796298a6707212c51
Gowtham Behara <gothsiddu@gmail.com>
2019-10-10 17:44:03 +0530

    Refactor import of auth components

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import Register from './auth/Register';
-import Login from './auth/Login';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 function App() {
   return (
