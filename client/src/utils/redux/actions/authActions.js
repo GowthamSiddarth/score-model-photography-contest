2357769da1cb2f3a17715154bcf38730d80681ba
Gowtham Behara <gothsiddu@gmail.com>
2019-10-10 14:03:58 +0530

    Define func to handle login action

--- client/src/utils/redux/actions/authActions.js
+++ client/src/utils/redux/actions/authActions.js
@@ -1,11 +1,36 @@
 import axios from 'axios';
-import { GET_ERRORS } from "./types";
+import setAuthToken from '../../auth/setAuthToken';
+import jwtDecode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
-const registerUser = (userData, history) => dispatch => {
+export const registerUser = (userData, history) => dispatch => {
     axios.post('/api/users/register', userData)
         .then(res => history.push('/login'))
         .catch(err => dispatch({
             type: GET_ERRORS,
             payload: err.response.data
         }));
+}
+
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            const { token } = res.data;
+            localStorage.setItem('jwtToken', token);
+
+            setAuthToken(token);
+            const decode = jwtDecode(token);
+            dispatch(setCurrentUser(decode));
+        })
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        }));
+}
+
+const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    };
 }
