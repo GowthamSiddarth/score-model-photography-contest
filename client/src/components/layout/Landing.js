d7027759e02e38e4934cf7e6940a1db610289112
Gowtham Behara <gothsiddu@gmail.com>
2019-10-11 12:26:38 +0530

    Connect Landing to store; Redirect to dashboard if user is authenticated

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
 
 class Landing extends Component {
+
+    componentDidMount() {
+        if (this.props.auth.isAuthenticated) {
+            this.props.history.push('/dashboard');
+        }
+    }
+
     render() {
         return (
             <div style={{ height: "75vh" }} className="container valign-wrapper">
@@ -47,4 +56,12 @@ class Landing extends Component {
     }
 }
 
-export default Landing;
+Landing.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(withRouter(Landing));
