79bcffcee63cb89296904c1ffbfc1f85a1a21069
Gowtham Behara <gothsiddu@gmail.com>
2019-10-12 16:12:19 +0530

    Recreate Navigation Bar using react-bootstrap components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,24 +1,63 @@
 import React, { Component } from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-class NavigationBar extends Component {
+const navIconTextItemStyle = {
+    fontFamily: "monospace",
+    display: "flex",
+    alignItems: "center"
+};
+
+class RegisterNavLink extends Component {
     render() {
-        const navBarBrandStyle = {
-            fontFamily: "monospace",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between"
-        };
-
         return (
-            <Navbar bg="red" className="hoverable">
-                <Navbar.Brand href="/" style={navBarBrandStyle} className="center brand-logo">
-                    <i className="material-icons">photo</i>
-                    MERN
-                </Navbar.Brand>
-            </Navbar>
+            <Nav.Link href="/register" style={navIconTextItemStyle} className="black-text"><i className="material-icons">how_to_reg</i>Register</Nav.Link>
         );
     }
 }
 
-export default NavigationBar;
+class LoginNavLink extends Component {
+    render() {
+        return (
+            <Nav.Link href="/login" style={navIconTextItemStyle} className="black-text"><i className="material-icons">navigate_next</i>Login</Nav.Link>
+        );
+    }
+}
+
+class NavigationBar extends Component {
+    render() {
+        return (
+            <div >
+                <Navbar className="hoverable py-0" fixed="top" collapseOnSelect expand="lg" style={{ height: "auto" }}>
+                    <Navbar.Brand href="/" style={navIconTextItemStyle}>
+                        <i className="material-icons">photo</i>
+                        SCORE YOUR PICS
+                </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbar-non-brand-items" />
+                    <Navbar.Collapse id="navbar-non-brand-items">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">How to Use</Nav.Link>
+                            <Nav.Link href="/">Contact Us</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <RegisterNavLink />
+                            <LoginNavLink />
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+NavigationBar.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps)(withRouter(NavigationBar));
