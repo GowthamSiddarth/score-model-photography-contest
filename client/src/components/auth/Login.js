57c157308ae7f3b84211ebcb67791df04e81cdb2
Gowtham Behara <gothsiddu@gmail.com>
2019-10-14 17:21:05 +0530

    Recreate Login component using react-bootstrap

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -8,6 +8,8 @@ import classnames from 'classnames';
 
 import { loginUser } from "../../utils/redux/actions/authActions";
 
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
 class Login extends Component {
     constructor() {
         super();
@@ -21,6 +23,7 @@ class Login extends Component {
 
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value });
+        delete this.state.errors[e.target.id];
     }
 
     onSubmit = e => {
@@ -52,64 +55,65 @@ class Login extends Component {
         const { errors } = this.state;
 
         return (
-            <div className="container">
-                <div className="row" style={{ paddingTop: "20px" }}>
-                    <div className="col s8 offset-s2">
-                        <Link to="/" className="btn-flat waves-effect">
+            <Container>
+                <Row>
+                    <Col className="my-auto" md={4} xs={0}>
+                        <Link to="/" className="btn-flat waves-effect" style={{ display: 'flex', justifyContent: 'center', textDecoration: 'none' }}>
                             <i className="material-icons left">keyboard_backspace</i>
                             Back to home
-                        </Link>
-                        <div className="col s12" style={{ paddingLeft: "10px" }}>
-                            <h4>
-                                <b>Login</b> below
-                            </h4>
-                            <p className="grey-text text-darken-1">
-                                Don't have an account?<Link to="/register"> Register</Link>
-                            </p>
-                        </div>
-                    </div>
-                    <form noValidate onSubmit={this.onSubmit}>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.email}
-                                error={errors.email}
-                                id="email"
-                                type="email"
-                                className={classnames('', { invalid: errors.email })}
-                            />
-                            <label htmlFor="email">Email</label>
-                            <span className="red-text">{errors.email}</span>
-                        </div>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.password}
-                                error={errors.password}
-                                id="password"
-                                type="password"
-                                className={classnames('', { invalid: errors.password })}
-                            />
-                            <label htmlFor="password">Password</label>
-                            <span className="red-text">{errors.password}</span>
-                        </div>
-                        <div className="col s8 offset-s2">
-                            <button
-                                style={{
-                                    width: "150px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    marginTop: "1rem"
-                                }}
-                                type="submit"
-                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >
-                                Log In
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                                </Link>
+                        <h4 className="text-center">
+                            <b>Login</b>
+                        </h4>
+                        <p className="grey-text text-darken-1 text-center">
+                            Don't have an account?<Link to="/register" style={{ textDecoration: 'none' }}> Register</Link>
+                        </p>
+                    </Col>
+                    <Col md={{ span: 5, offset: 2 }} style={{ paddingTop: "30px" }}>
+                        <Form noValidate onSubmit={this.onSubmit}>
+                            <Form.Group controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    type="email"
+                                    className={classnames('', { invalid: errors.email })}
+                                    isInvalid={!!errors.email}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    type="password"
+                                    className={classnames('', { invalid: errors.password })}
+                                    isInvalid={!!errors.password}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                            </Form.Group>
+                            <div class="text-center">
+                                <Button
+                                    variant="outline-primary"
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="waves-effect waves-light hoverable"
+                                >
+                                    Sign In
+                                </Button>
+                            </div>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
