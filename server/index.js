08127e935ffedcd88c2661e016633cdb55fe4a5b
Gowtham Behara <gothsiddu@gmail.com>
2019-10-14 19:38:52 +0530

    Change PORT number for backend server

--- server/index.js
+++ server/index.js
@@ -20,6 +20,6 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB connection successful"))
     .catch(err => console.log(err));
 
-const SERVER_PORT = process.env.PORT || 5000;
+const SERVER_PORT = process.env.PORT || 7000;
 
 app.listen(SERVER_PORT, () => console.log(`Server Up and running on port ${SERVER_PORT}`));
