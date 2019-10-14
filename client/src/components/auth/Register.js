db0d392b04491c2fa78bed14d6cb2a2e8a63e5ba
Gowtham Behara <gothsiddu@gmail.com>
2019-10-14 16:58:17 +0530

    Redefine Register component with react-bootstrap components

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -7,6 +7,8 @@ import { registerUser } from "../../utils/redux/actions/authActions";
 import { withRouter } from "react-router-dom";
 import classnames from "classnames";
 
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
 class Register extends Component {
     constructor() {
         super();
@@ -22,6 +24,7 @@ class Register extends Component {
 
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value });
+        delete this.state.errors[e.target.id];
     };
 
     onSubmit = e => {
@@ -55,88 +58,89 @@ class Register extends Component {
         const { errors } = this.state;
 
         return (
-            <div className="container">
-                <div className="row" style={{ paddingTop: "20px" }}>
-                    <div className="col s8 offset-s2">
-                        <Link to="/" className="btn-flat waves-effect">
+            <Container>
+                <Row>
+                    <Col className="my-auto" md={4} xs={0}>
+                        <Link to="/" className="btn-flat waves-effect" style={{ display: 'flex', justifyContent: 'center' }}>
                             <i className="material-icons left">keyboard_backspace</i>
                             Back to home
-                        </Link>
-                        <div className="col s12">
-                            <h4>
-                                <b>Register</b> below
-                            </h4>
-                            <p className="grey-text text-darken-1">
-                                Already have an account?<Link to="/login"> Login</Link>
-                            </p>
-                        </div>
-                    </div>
-                    <form noValidate onSubmit={this.onSubmit}>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.name}
-                                error={errors.name}
-                                id="name"
-                                type="text"
-                                className={classnames('', { invalid: errors.name })}
-                            />
-                            <label htmlFor="name">Name</label>
-                            <span className="red-text">{errors.name}</span>
-                        </div>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.email}
-                                error={errors.email}
-                                id="email"
-                                type="email"
-                                className={classnames('', { invalid: errors.email })}
-                            />
-                            <label htmlFor="email">Email</label>
-                            <span className="red-text">{errors.email}</span>
-                        </div>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.password}
-                                error={errors.password}
-                                id="password"
-                                type="password"
-                                className={classnames('', { invalid: errors.password })}
-                            />
-                            <label htmlFor="password">Password</label>
-                            <span className="red-text">{errors.password}</span>
-                        </div>
-                        <div className="input-field col s8 offset-s2">
-                            <input
-                                onChange={this.onChange}
-                                value={this.state.confirm_password}
-                                error={errors.confirm_password}
-                                id="confirm_password"
-                                type="password"
-                                className={classnames('', { invalid: errors.confirm_password })}
-                            />
-                            <label htmlFor="confirm_password">Confirm Password</label>
-                            <span className="red-text">{errors.confirm_password}</span>
-                        </div>
-                        <div className="col s8 offset-s2" style={{ paddingLeft: "10px" }}>
-                            <button
-                                style={{
-                                    width: "150px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    marginTop: "1rem"
-                                }}
-                                type="submit"
-                                className="btn btn-large waves-effect waves-light hoverable green accent-4"
-                            >
-                                Sign Up
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                                </Link>
+                        <h4 className="text-center">
+                            <b>Register</b>
+                        </h4>
+                        <p className="grey-text text-darken-1 text-center">
+                            Already have an account?<Link to="/login"> Login</Link>
+                        </p>
+                    </Col>
+                    <Col md={{ span: 5, offset: 2 }} style={{ paddingTop: "30px" }}>
+                        <Form noValidate onSubmit={this.onSubmit}>
+                            <Form.Group controlId="name">
+                                <Form.Label htmlFor="name">Name</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.name}
+                                    error={errors.name}
+                                    type="text"
+                                    className={classnames('input-field', { invalid: errors.name })}
+                                    isInvalid={!!errors.name}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.email}
+                                    error={errors.email}
+                                    type="email"
+                                    className={classnames('', { invalid: errors.email })}
+                                    isInvalid={!!errors.email}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.password}
+                                    error={errors.password}
+                                    type="password"
+                                    className={classnames('', { invalid: errors.password })}
+                                    isInvalid={!!errors.password}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+                            </Form.Group>
+                            <Form.Group controlId="confirm_password">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control
+                                    onChange={this.onChange}
+                                    value={this.state.confirm_password}
+                                    error={errors.confirm_password}
+                                    type="password"
+                                    className={classnames('', { invalid: errors.confirm_password })}
+                                    isInvalid={!!errors.confirm_password}
+                                />
+                                <Form.Control.Feedback type="invalid">{errors.confirm_password}</Form.Control.Feedback>
+                            </Form.Group>
+                            <div class="text-center">
+                                <Button
+                                    variant="outline-success"
+                                    style={{
+                                        width: "150px",
+                                        borderRadius: "3px",
+                                        letterSpacing: "1.5px",
+                                        marginTop: "1rem"
+                                    }}
+                                    type="submit"
+                                    className="waves-effect waves-light hoverable black-text accent-4"
+                                >
+                                    Sign Up
+                                </Button>
+                            </div>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
