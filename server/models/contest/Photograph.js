c0132450a7f4c6275dac4ff3f0eefbe6aed2aec2
Gowtham Behara <gothsiddu@gmail.com>
2019-10-18 12:29:06 +0530

    Create schema for Photograph model

---
+++ server/models/contest/Photograph.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PhotographSchema = new Schema({
+    image: {
+        type: Schema.Types.Buffer,
+        required: true
+    },
+    submitted_by: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    submitted_on: {
+        type: Schema.Types.Date,
+        default: Date.now
+    },
+    description: {
+        type: Schema.Types.String,
+        required: true
+    },
+    contest_id: {
+        type: Schema.Types.ObjectId,
+        required: true
+    }
+});
+
+module.exports = mongoose.model("photographs", PhotographSchema);
