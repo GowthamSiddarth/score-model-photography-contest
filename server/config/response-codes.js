b8c5bf271e0cf5acf4d7ad68e5b1088903f9e9be
Gowtham Behara <gothsiddu@gmail.com>
2019-10-21 13:08:55 +0530

    Add diff types of response codes

---
+++ server/config/response-codes.js
@@ -0,0 +1,11 @@
+const TWO_HUNDRED = 200;
+const FOUR_HUNDRED = 400;
+const FOUR_O_FOUR = 404;
+const FIVE_HUNDRED = 500;
+
+module.exports = {
+    TWO_HUNDRED,
+    FOUR_HUNDRED,
+    FOUR_O_FOUR,
+    FIVE_HUNDRED
+};
