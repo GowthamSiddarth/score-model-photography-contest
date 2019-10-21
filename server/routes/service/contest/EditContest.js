21297b7aa0d2e32d8012d0fcf1a5fe40eaf1e3a4
Gowtham Behara <gothsiddu@gmail.com>
2019-10-21 17:22:51 +0530

    Implement service for edit contest request

---
+++ server/routes/service/contest/EditContest.js
@@ -0,0 +1,32 @@
+const validateContest = require('../../../helper/validation/contest');
+
+const responseCode = require('../../../config/response-codes');
+
+const { messageResponse, objectResponse } = require('../../../helper/response-entity/response-body');
+
+const Contest = require('../../../models/contest/Contest');
+
+const editContest = (contestOldName, contestNewName, editedBy) => {
+    return new Promise((resolve, reject) => {
+        let { errors, isValid } = validateContest(contestOldName, editedBy, contestNewName);
+
+        if (!isValid) {
+            resolve(objectResponse(false, errors, responseCode.FOUR_HUNDRED));
+        }
+
+        Contest.findOneAndUpdate({ name: contestOldName }, { 
+            name: contestNewName, 
+            last_edited_by: editedBy,
+            last_edited_on: Date.now()
+        }, { 
+            new: true,
+            useFindAndModify: false
+        }, (err, contest) => {
+            if (err) reject(err);
+            else if (!contest) resolve(messageResponse(false, `No contest found with name ${contestOldName}`, responseCode.FOUR_HUNDRED));
+            else resolve(messageResponse(true, `Contest name updated as ${contest.name}`));
+        });
+    });
+}
+
+module.exports = editContest;
