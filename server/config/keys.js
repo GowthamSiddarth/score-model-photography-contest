fadd78ae85a78ff2623fca6dadaa808c94127725
Gowtham Behara <gothsiddu@gmail.com>
2019-10-23 19:59:03 +0530

    Add loc for photograph submission through file upload

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: "mongodb://localhost:27017/score-model-photography-contest",
-    secretOrKey: "28A66AE7F79DEBE9A66F1B53D491C"
+    secretOrKey: "28A66AE7F79DEBE9A66F1B53D491C",
+    uploadLoc: "assets/images"
 };
