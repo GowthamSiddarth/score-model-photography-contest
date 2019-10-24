ef18d06a546355626a5788777bcd43ca23f9771a
Gowtham Behara <gothsiddu@gmail.com>
2019-10-24 19:17:38 +0530

    Refactor messageResponse

--- server/helper/request-entity/request-headers.js
+++ server/helper/request-entity/request-headers.js
@@ -1,27 +1,27 @@
 const jwt = require('jsonwebtoken');
-const { simpleMessageResponse } = require('../response-entity/response-body');
+const { messageResponse } = require('../response-entity/response-body');
 
 const secretOrKey = require('../../config/keys').secretOrKey;
 
 const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
     if (!token) {
-        return res.status(403).json(simpleMessageResponse(false, "Unauthorized User!"));
+        return res.status(403).json(messageResponse(false, "Unauthorized User!"));
     }
 
     jwt.verify(token, secretOrKey, (err, decoded) => {
         if (err) {
-            res.status(500).json(simpleMessageResponse(false, "Failed to authenticate token"));
+            res.status(500).json(messageResponse(false, "Failed to authenticate token"));
         }
 
         const authUser = decoded.isAdmin ? require('../../models/users/Admin') : require('../../models/users/User');
         authUser.findById(decoded.id, { password: false }, (err, _authUser) => {
             if (err) {
-                res.status(500).json(simpleMessageResponse(false, "There was a problem finding User. Please contact administrator"));
+                res.status(500).json(messageResponse(false, "There was a problem finding User. Please contact administrator"));
             }
 
             if (!_authUser) {
-                res.status(404).json(simpleMessageResponse(false, "User not found!"));
+                res.status(404).json(messageResponse(false, "User not found!"));
             }
 
             next();
