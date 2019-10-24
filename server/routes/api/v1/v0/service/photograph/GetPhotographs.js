1958b3d7d8dbdb036d313ce14e3cb868b569ef52
Gowtham Behara <gothsiddu@gmail.com>
2019-10-24 19:20:07 +0530

    Define service to read content from image saved

---
+++ server/routes/api/v1/v0/service/photograph/GetPhotographs.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const { objectResponse } = require('../../../../../../helper/response-entity/response-body');
+const readFile = require('../../../../../../helper/files/readFile');
+
+const imagesLoc = require('../../../../../../config/keys').uploadLoc;
+
+const Contest = require('../../../../../../models/contest/Contest');
+const Photograph = require('../../../../../../models/contest/Photograph');
+
+const getPhotographs = (contestName) => {
+    return new Promise((resolve, reject) => {
+        Contest.findOne({ name: contestName }, { photographs: true })
+            .then(contest => {
+                Promise.all(contest.photographs.map(photograph_id =>
+                    Photograph.findById(photograph_id, { filename: true })
+                        .then(photograph => photograph.image)
+                        .catch(err => console.log(err))
+                ))
+                    .then(images => resolve(objectResponse(true, { photographs: images })))
+                    .catch(err => reject(err));
+            })
+            .catch(err => reject(err));
+    });
+};
+
+const readPhotograph = (filename) => {
+    return new Promise((resolve, reject) => {
+        const filePath = path.join(imagesLoc, filename);
+        readFile(filePath).then(data => resolve(data)).catch(err => reject(err));
+    });
+};
+
+module.exports = {
+    getPhotographs,
+    readPhotograph
+};
